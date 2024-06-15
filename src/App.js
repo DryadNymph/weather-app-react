8a189c9781e8a2a33e7cd015d8692c258210e9db
DryadNymph <mymind203@yahoo.com>
2024-06-15 18:12:21 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project is coded by{" "}
           <a href="https://www.linkedin.com/in/lali-nibladze146151019960916/">
