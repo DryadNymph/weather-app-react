5bdea719c57268ab8a1227fd07862271e9473258
DryadNymph <mymind203@yahoo.com>
2024-06-15 16:40:52 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a href="https://www.linkedin.com/in/lali-nibladze146151019960916/">
-          Lali Nibladze
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/DryadNymph/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-spurced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a href="https://www.linkedin.com/in/lali-nibladze146151019960916/">
+            Lali Nibladze
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/DryadNymph/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-spurced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
